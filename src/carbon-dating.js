2a2ee85d03fd615a470282dfd509d74dab356aad
Daniil Denisov <stxg9d@gmail.com>
2021-03-29 22:11:01 +0300

    feat: realised carbon-dating function

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -4,5 +4,7 @@ const MODERN_ACTIVITY= 15;
 const HALF_LIFE_PERIOD= 5730;
 
 module.exports = function dateSample(sampleActivity) {
-  
+    if (Number.isNaN(sampleActivity) == false || typeof(sampleActivity) != 'string') return false;
+    let k = 0.693 / HALF_LIFE_PERIOD;;
+    return res = Math.ceil(Math.log(MODERN_ACTIVITY / (+sampleActivity)) / k);
 };
